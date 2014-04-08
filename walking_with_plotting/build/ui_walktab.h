f8f25ab597e21821cd079e133fa025282044663c
Can Erdogan <canerdogan.89@gmail.com>
2014-04-08 09:53:08 -0400

    Added the walking plugin to add plotting data

---
+++ walking_with_plotting/build/ui_walktab.h
@@ -0,0 +1,120 @@
+/********************************************************************************
+** Form generated from reading UI file 'walktab.ui'
+**
+** Created: Tue Apr 8 00:32:16 2014
+**      by: Qt User Interface Compiler version 4.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WALKTAB_H
+#define UI_WALKTAB_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QDockWidget>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_WalkTabWidget
+{
+public:
+    QWidget *dockWidgetContents;
+    QHBoxLayout *horizontalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QLabel *label_2;
+    QLabel *label;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer_2;
+    QPushButton *setStart;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *setGoal;
+    QSpacerItem *horizontalSpacer_3;
+
+    void setupUi(QDockWidget *WalkTabWidget)
+    {
+        if (WalkTabWidget->objectName().isEmpty())
+            WalkTabWidget->setObjectName(QString::fromUtf8("WalkTabWidget"));
+        WalkTabWidget->resize(663, 182);
+        dockWidgetContents = new QWidget();
+        dockWidgetContents->setObjectName(QString::fromUtf8("dockWidgetContents"));
+        horizontalLayout_2 = new QHBoxLayout(dockWidgetContents);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        label_2 = new QLabel(dockWidgetContents);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        verticalLayout->addWidget(label_2);
+
+        label = new QLabel(dockWidgetContents);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        verticalLayout->addWidget(label);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_2);
+
+        setStart = new QPushButton(dockWidgetContents);
+        setStart->setObjectName(QString::fromUtf8("setStart"));
+        setStart->setMinimumSize(QSize(100, 50));
+
+        horizontalLayout->addWidget(setStart);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        setGoal = new QPushButton(dockWidgetContents);
+        setGoal->setObjectName(QString::fromUtf8("setGoal"));
+        setGoal->setMinimumSize(QSize(100, 50));
+
+        horizontalLayout->addWidget(setGoal);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_3);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        horizontalLayout_2->addLayout(verticalLayout);
+
+        WalkTabWidget->setWidget(dockWidgetContents);
+
+        retranslateUi(WalkTabWidget);
+
+        QMetaObject::connectSlotsByName(WalkTabWidget);
+    } // setupUi
+
+    void retranslateUi(QDockWidget *WalkTabWidget)
+    {
+        WalkTabWidget->setWindowTitle(QApplication::translate("WalkTabWidget", "Walk Tab", 0, QApplication::UnicodeUTF8));
+        label_2->setText(QApplication::translate("WalkTabWidget", "Step1. Load the scene from data folder (04-world-Automatize.urdf)", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("WalkTabWidget", "Step2. Press \"Set Start\" and \"Set Goal\" and then press start button(green arrow) on the menu", 0, QApplication::UnicodeUTF8));
+        setStart->setText(QApplication::translate("WalkTabWidget", "Set Start", 0, QApplication::UnicodeUTF8));
+        setGoal->setText(QApplication::translate("WalkTabWidget", "Set Goal", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class WalkTabWidget: public Ui_WalkTabWidget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WALKTAB_H
---
+++ walking_with_plotting/src/helpers.cpp
@@ -0,0 +1,356 @@
+/*
+ * Copyright (c) 2014, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author: Can Ergodan <cerdogan3@gatech.edu>, Sungmoon Joo <sungmoon.joo@gmail.com>
+ * Date: April 2014
+ *
+ * Humanoid Robotics Lab      Georgia Institute of Technology
+ * Director: Mike Stilman     http://www.golems.org
+ *
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of the Humanoid Robotics Lab nor the names of
+ *     its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written
+ *     permission
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "helpers.h"
+
+double obstacleLoc = 0.2;
+
+/* ********************************************************************************************* */
+void lift () {
+	vector <int> z_idx;
+	z_idx.push_back(5);
+	Eigen::VectorXd z = hubo->getConfig(z_idx);
+	z(0) += 1e-3;
+	hubo->setConfig(z_idx, z);
+}
+
+/* ********************************************************************************************* */
+bool descend (size_t step) {
+
+	cout << "descend step: " << step << endl;
+	cout << "descend foot: " << ((foot == LEFT) ? "LEFT" : "RIGHT") << endl;
+
+	// Get the feet
+	Eigen::Vector3d com = hubo->getWorldCOM();
+	BodyNode* leftFoot = hubo->getBodyNode("leftFoot"), *rightFoot = hubo->getBodyNode("rightFoot");
+	BodyNode* lead = (foot == LEFT) ? leftFoot : rightFoot, *stable= (foot != LEFT) ? leftFoot : rightFoot;
+
+	switch(step) {
+
+		case 0: {
+			nextpos = Eigen::Vector3d(obstacleLoc + 0.10, ((foot == LEFT) ? 0.09 : -0.09), (lastHeight + 0.03));
+			mode = ToPos;
+		} break;
+		case 1: {
+			foot = otherFoot(foot);
+			mode = Shrink;
+			motionLimit = com(2) - 0.2;
+		} break;
+		case 2: {
+			foot = otherFoot(foot);
+			mode = Down;
+			motionLimit = currentHeight + 0.010;
+		} break;
+		case 3: {
+			mode = Shift;
+		} break;
+		case 4: {
+			lift();
+			foot = otherFoot(foot);
+			mode = Up;
+			motionLimit = lastHeight + 0.03;
+		} break;
+		case 5: {
+			mode = Forward;
+			Eigen::Vector3d temp = stable->getWorldTransform().translation();
+			motionLimit = temp(0) - 0.03;
+		} break;
+		case 6: {
+			mode = Down;
+			motionLimit = currentHeight + 0.05;
+		} break;
+		case 7: {
+			return true;
+		} break;
+	};
+
+	lastId = step;
+	return false;
+}
+
+/* ********************************************************************************************* */
+bool climb (size_t step) {
+
+	cout << "climb foot: " << ((foot == LEFT) ? "LEFT" : "RIGHT") << endl;
+	Eigen::Vector3d com = hubo->getWorldCOM();
+
+	// Get the feet
+	BodyNode* leftFoot = hubo->getBodyNode("leftFoot"), *rightFoot = hubo->getBodyNode("rightFoot");
+	BodyNode* lead = (foot == LEFT) ? leftFoot : rightFoot, *stable= (foot != LEFT) ? leftFoot : rightFoot;
+
+	// Decide on the actions
+	pc(step);
+	switch(step) {
+
+		// Bring left more forward - match height of the obstacle 
+		case 0: {
+			mode = Up;
+			motionLimit = boxHeight + 0.03;
+		} break;
+		case 1: {
+			Eigen::Vector3d temp = stable->getWorldTransform().translation();
+			nextpos = Eigen::Vector3d(temp(0), ((foot == LEFT) ? 0.09 : -0.09), (boxHeight + 0.03));
+			mode = ToPos;
+		} break;
+		case 2: {
+			nextpos = Eigen::Vector3d(obstacleLoc + 0.05, ((foot == LEFT) ? 0.09 : -0.09), (boxHeight + 0.03));
+			mode = ToPos;
+		} break;
+		case 3: {
+			mode = Backward;
+			foot = otherFoot(foot);
+			motionLimit = com(0) + 0.05;
+		} break;
+		case 4: {
+			foot = otherFoot(foot);
+			mode = Down;
+			motionLimit = boxHeight + 0.005;
+		} break;
+		case 5: {
+			mode = Shift;
+		} break;
+		case 6: {
+			lift();
+			foot = otherFoot(foot);
+			mode = Up;
+			motionLimit = boxHeight + 0.03;
+		} break;
+		case 7: {
+			Eigen::Vector3d temp = stable->getWorldTransform().translation();
+			nextpos = Eigen::Vector3d(temp(0), ((foot == LEFT) ? 0.09 : -0.09), (boxHeight + 0.03));
+			mode = ToPos;
+		} break;
+		case 8: {
+			foot = otherFoot(foot);
+			mode = PushDown;
+			motionLimit = 0.73 + currentHeight;
+		} break;
+		case 9: {
+			return true;
+		} break;
+	};
+
+	lastId = step;
+	return false;
+}
+
+/* ********************************************************************************************* */
+bool walk (size_t step) {
+
+	static const double footLength = 0.16;
+	cout << "walk step: " << step << endl;
+	cout << "walk foot: " << ((foot == LEFT) ? "LEFT" : "RIGHT") << endl;
+
+	// Declare the walk done if step was already shortened and repeat is asked
+	static size_t wayPointIdx = 0;
+	static bool shortenedStep = false;
+	static bool liftedFoot = false;
+	if((step == 0) && shortenedStep && liftedFoot) {
+		shortenedStep = liftedFoot = false;
+		lastHeight = currentHeight;
+		currentHeight = wayPoints[wayPointIdx](1); 
+		cout << "currentHeight: " << currentHeight << ", lastHeight: " << lastHeight << endl;
+		return true;
+	} 
+
+	// Get the feet
+	BodyNode* leftFoot = hubo->getBodyNode("leftFoot"), *rightFoot = hubo->getBodyNode("rightFoot");
+	BodyNode* lead = (foot == LEFT) ? leftFoot : rightFoot, *stable= (foot != LEFT) ? leftFoot : rightFoot;
+
+	// Decide on the actions
+	switch(step) {
+
+		// Move lead foot forward
+		case 0: {
+
+			// Decide on the step size
+			Eigen::Vector3d temp = stable->getWorldTransform().translation();
+			double nextWayPoint;
+			for(size_t i = 0; i < wayPoints.size(); i++) {
+				double dist = wayPoints[i](0) - (temp(0) + footLength);
+				if(dist < 0.0) continue;
+				if(dist < 0.25) {
+					wayPointIdx = i;
+					obstacleLoc = wayPoints[i](0);
+					stepSize = dist - 0.06;	
+					shortenedStep = true;
+					pc(stepSize);
+					break;
+				}
+			}
+
+			// Set the inputs
+			mode = Forward;
+			motionLimit = temp(0) + stepSize;
+		} break;
+
+		// Put lead down
+		case 1: {
+			mode = Down;
+			motionLimit = 0.020 + currentHeight;
+		} break;
+
+		// Shift mass
+		case 2: {
+			lift();
+			mode = Shift;
+		} break;
+
+		// Raise back 
+		case 3: {
+			lift();
+			mode = Up;
+			motionLimit = 0.05 + currentHeight;
+			foot = otherFoot(foot);
+			liftedFoot = true;
+		} break;
+	};
+
+	lastId = step;
+	return false;
+}
+
+/* ********************************************************************************************* */
+bool prepare (size_t step) {
+
+	switch(step) {
+
+		case 0: {
+			static const double angle = (-25.0 / 180.0) * M_PI;
+			desiredDofs[RHP] = angle;
+			desiredDofs[RKP] = -2*angle;
+			desiredDofs[RAP] = angle;
+			desiredDofs[LHP] = angle;
+			desiredDofs[LKP] = -2*angle;
+			desiredDofs[LAP] = angle;
+			mode = NONE;
+		} break;
+		case 3:
+		case 2:
+		case 1: {
+			static const double angle = (-3.3 / 180.0) * M_PI;
+			desiredDofs[RHR] += -angle;
+			desiredDofs[RAR] += angle;
+			desiredDofs[LHR] += -angle;
+			desiredDofs[LAR] += angle;
+			mode = NONE;
+		} break;
+		case 4: {
+			mode = Up;
+			foot = LEFT;
+			motionLimit = 0.05 + currentHeight;
+		} break;
+		case 5: {
+			cout << "Prepare returning true" << endl;
+			return true;
+		} break;
+	};
+
+	lastId = step;
+	return false;
+}
+
+/* ********************************************************************************************* *
+void printContactForces () {
+
+	// Print the average forces under the feet
+	static size_t counter = 0;
+	Eigen::Vector3d leftAve = Eigen::Vector3d::Zero(), rightAve = Eigen::Vector3d::Zero();
+	int nContacts = mWorld->getConstraintHandler()->getCollisionDetector()->getNumContacts();
+	int numLeftContacts = 0, numRightContacts = 0;
+	BodyNode* leftAnkle = hubo->getBodyNode("Body_LAR"), *rightAnkle = hubo->getBodyNode("Body_RAR");
+	if(counter % 100 == 0) printf("\n");
+	for (int k = 0; k < nContacts; k++) {
+		dart::collision::Contact contact = mWorld->getConstraintHandler()->getCollisionDetector()->getContact(k);
+		if(0 && (counter % 100 == 0)) {
+			printf("'%s' vs. '%s': ", contact.collisionNode1->getBodyNode()->getName().c_str(), 
+				contact.collisionNode2->getBodyNode()->getName().c_str());
+			cout << contact.force.transpose() << endl;
+		}
+		if(contact.collisionNode1->getBodyNode() == leftAnkle || contact.collisionNode2->getBodyNode() == leftAnkle) {
+			if(contact.force.norm() > 0.1) {
+				leftAve += contact.force;
+				numLeftContacts++;
+			}
+		}   
+		if(contact.collisionNode1->getBodyNode() == rightAnkle || contact.collisionNode2->getBodyNode() == rightAnkle) {
+			if(contact.force.norm() > 0.1) {
+				numRightContacts++;
+				rightAve += contact.force;
+			}
+		}   
+	}  
+	if(numLeftContacts != 0) leftAve /= numLeftContacts;
+	if(numRightContacts != 0) rightAve /= numRightContacts;
+	if(counter++ % 100 == 0) {
+		printf("\n# contacts: %d, # left: %d, # right: %d\n", nContacts, numLeftContacts, numRightContacts);
+		pv(leftAve);
+		pv(rightAve);
+		cout << "average z: " << (leftAve(2) + rightAve(2)) << endl;
+	}
+	
+	// Clear the average force if it is negative
+	if(leftAve(2) < 0.0) {
+		leftAnkle->clearContactForces();
+		leftAnkle->clearExternalForces();
+	}
+	if(rightAve(2) < 0.0) {
+		rightAnkle->clearContactForces();
+		rightAnkle->clearExternalForces();
+	}
+}
+
+/* ********************************************************************************************* */
+std::string modeStr(Mode mode) {
+	switch(mode) {
+		case Forward: return "Forward";
+		case Up: return "Up";
+		case Backward: return "Backward";
+		case Down: return "Down";
+		case ToPos: return "ToPos";
+		case Shift: return "Shift";
+		case PushDown: return "PushDown";
+		case Shrink: return "Shrink";
+		case NONE: return "NONE";
+	}; 
+}
