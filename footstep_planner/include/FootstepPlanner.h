f4320150f8773c38daf3f3a369736adaaa591495
mohit <mohit@mumbojumbo.(none)>
2014-04-15 11:11:05 -0400

    Checking in changes to the definition of weighted a star. Was creating troubles.

--- footstep_planner/include/FootstepPlanner.h
+++ footstep_planner/include/FootstepPlanner.h
@@ -127,7 +127,7 @@ class FootstepPlanner
         FootLocation _generateRandomFootConfig(int previousFootIndex, int nextFootIndex, const std::vector<FootConstraint>& constraints, fsp::FootLocation flStanceFoot, Eigen::Vector2d randomPoint);
 				double get_euclid_distance(FootLocationNode* node, FootLocation footLocation);
                 double get_euclid_distance(FootLocationNode* node, Eigen::Vector2d footLocation);
-				void find_path_using_weighted_a_star(FootLocationNode* parent, FootLocationNode* goal, int desired_weight, vector<Line> obstacles);
+                bool find_path_using_weighted_a_star(FootLocationNode* parent, FootLocationNode* goal, int desired_weight, vector<Line> obstacles);
 				FootLocationNode* get_random_goal(FootLocationNode* currentLocation, int radius, vector<Line> obstacles); 
 				vector<FootLocationNode*> get_possible_configurations(FootLocationNode* currentLocation, int radius, vector<Line> obstacles, int num_config);
         double _minimumRandomX;
